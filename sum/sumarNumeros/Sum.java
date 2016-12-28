846da645e6541b4d62b9678b83e1b3ba0ac40e10
Sara Chávez <sarachavezsoftware@gmail.com>
2016-12-28 17:24:26 +0100

    Función suma de n números (positivos y negativos) [sumNumeros(int... numbers)], junto con método test [testSumNumerosPosAndNeg()].

--- sum/sumarNumeros/Sum.java
+++ sum/sumarNumeros/Sum.java
@@ -2,11 +2,20 @@ package sumarNumeros;
 
 public class Sum {
 	
-	public static int sum(int a, int b, int c) {
+	public static int sum3numeros(int a, int b, int c) {
 		
 		int result = 0;
 		result = a + b + c;
 		return result;
 	}
 
+	public static int sumNumeros(int... numbers) {
+		
+		int result = 0;
+		for (int n :numbers){
+			
+			result += n;
+		}
+		return result;
+	}
 }
--- test/testSumarNumeros/TestSum.java
+++ test/testSumarNumeros/TestSum.java
@@ -10,9 +10,18 @@ public class TestSum {
 	
 	@Test
 	public void testSum2NumPosAndNeg() {
-		assertEquals(6, Sum.sum(2, 1, 3));
-		assertEquals(-11, Sum.sum(-10, -1, 0));
-		assertEquals(-14, Sum.sum(-10, -1, -3));
+		assertEquals(6, Sum.sum3numeros(2, 1, 3));
+		assertEquals(-11, Sum.sum3numeros(-10, -1, 0));
+		assertEquals(-14, Sum.sum3numeros(-10, -1, -3));
+
+	}
+	
+	@Test
+	public void testSumNumerosPosAndNeg() {
+		assertEquals(11, Sum.sumNumeros(1, 2, 3, 5));
+		assertEquals(0, Sum.sumNumeros(1, 2, -3, 0));
+		assertEquals(-8, Sum.sumNumeros(-1, -2, -3, -2));
+
 
 	}
 
